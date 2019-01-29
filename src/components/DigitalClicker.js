8b6b7bb0b29aab46524019300253b8ae60d4da57
Jesus Escalona <jesusescalona@me.com>
2019-01-29 10:54:37 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,17 @@
-// Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+class DigitalClicker extends Component {
+  state = {
+    timesClicked: 0
+  }
+
+  render() {
+    const { timesClicked } = this.state
+    return (
+      <button onClick={() => this.setState({timesClicked: timesClicked+1})}>{timesClicked}</button>
+      )
+  }
+
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,27 @@
-// Code YouTubeDebugger Component Here
+import React, {Component} from "react"
+
+class YouTubeDebugger extends Component {
+  state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: '1080p'
+      }
+    }
+  }
+
+  render() {
+    const { settings } = this.state
+    return (
+      <div>
+        <button className="bitrate" onClick={() => this.setState({settings: {...settings, bitrate: 12} })}>{settings.bitrate}</button>
+        <button className="resolution" onClick={() => this.setState({settings: { ...settings, video: { ...settings.video, resolution: '720p'}}})}>{settings.video.resolution}</button>
+      </div>
+      )
+  }
+
+}
+
+export default YouTubeDebugger
